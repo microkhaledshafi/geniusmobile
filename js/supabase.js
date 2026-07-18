b0f25ec151843f98ab693d26e164413491028676
microkhaledshafi <microkhaledshafi@yahoo.com>
2026-07-18 16:12:52 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,10 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js'
 
+const supabaseUrl = "https://cxssryhesrgomcdxddwn.supabase.co"
+
+const supabaseKey = "sb_publishable_QiNQzM7U5l6mJpbHIN4UJQ_nv_90lED"
+
+export const supabase = createClient(
+    supabaseUrl,
+    supabaseKey
+)
