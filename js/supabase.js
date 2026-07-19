7a5881c28b23271254db528d9f3fce2eb01ee6ab
microkhaledshafi <microkhaledshafi@yahoo.com>
2026-07-19 20:19:33 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,10 +1,9 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js'
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const supabaseUrl = "https://cxssryhesrgomcdxddwn.supabase.co"
-
-const supabaseKey = "sb_publishable_QiNQzM7U5l6mJpbHIN4UJQ_nv_90lED"
+const SUPABASE_URL = "https://cxssryhesrgomcdxddwn.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_QiNQzM7U5l6mJpbHIN4UJQ_nv_90lED";
 
 export const supabase = createClient(
-    supabaseUrl,
-    supabaseKey
-)
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
