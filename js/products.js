2e5da611963c8a6c3c52eb194f94ee6deb57e95a
microkhaledshafi <microkhaledshafi@yahoo.com>
2026-07-19 19:54:33 +0530

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,25 +1,37 @@
 import { supabase } from "./supabase.js";
 
+const searchInput = document.getElementById("searchProduct");
+
 const table = document.getElementById("productTable");
 
-async function loadProducts() {
+async function loadProducts(search = "") {
 
-    const { data, error } = await supabase
-
+    let query = supabase
         .from("products")
-
         .select(`
             id,
             product_code,
             product_name,
+            barcode,
             mrp,
             selling_rate,
             quantity,
             gst,
             categories(category_name)
         `)
-
         .order("product_name");
+
+    if (search !== "") {
+
+        query = query.or(
+            `product_name.ilike.%${search}%,
+             product_code.ilike.%${search}%,
+             barcode.ilike.%${search}%`
+        );
+
+    }
+
+    const { data, error } = await query;
 
     if (error) {
 
@@ -66,5 +78,9 @@ async function loadProducts() {
     });
 
 }
+searchInput.addEventListener("keyup", () => {
 
+    loadProducts(searchInput.value.trim());
+
+});
 loadProducts();
