b1e32d4d96972459746108f031fd6bda3d180600
microkhaledshafi <microkhaledshafi@yahoo.com>
2026-07-23 00:22:23 +0530

    Create config.js

---
+++ billing/js/config.js
@@ -0,0 +1,17 @@
+export const CONFIG = {
+
+    COMPANY_NAME: "Genius Scientific",
+
+    CURRENCY: "₹",
+
+    DECIMAL_PLACES: 2,
+
+    LOW_STOCK_LIMIT: 5,
+
+    GST_MODE: "CGST",
+
+    DATE_FORMAT: "DD-MM-YYYY",
+
+    VERSION: "2.0.0"
+
+};
