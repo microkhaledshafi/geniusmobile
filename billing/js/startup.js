6604acde1a5a546fd194c0fa4e19139191c3c5bb
microkhaledshafi <microkhaledshafi@yahoo.com>
2026-07-23 00:30:03 +0530

    Update startup.js

--- billing/js/startup.js
+++ billing/js/startup.js
@@ -1,11 +1,12 @@
 import { CONFIG } from "./config.js";
+import { initInvoice } from "./invoice.js";
 
 export async function initBillingApp() {
 
     console.log(
-
         `${CONFIG.COMPANY_NAME} Billing Started`
-
     );
+
+    initInvoice();
 
 }
