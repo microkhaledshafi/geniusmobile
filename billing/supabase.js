3f3bb6e8b088477e5ffddb2c51905700dd70fd18
microkhaledshafi <microkhaledshafi@yahoo.com>
2026-07-26 12:51:14 +0530

    Update supabase.js

--- billing/supabase.js
+++ billing/supabase.js
@@ -8,7 +8,7 @@ Supabase Configuration
 import { createClient } from "https://esm.sh/@supabase/supabase-js";
 
 const SUPABASE_URL = "https://cxssryhesrgomcdxddwn.supabase.co";
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+const SUPABASE_ANON_KEY = "sb_publishable_QiNQzM7U5l6mJpbHIN4UJQ_nv_90lED";
 
 export const supabase = createClient(
     SUPABASE_URL,
